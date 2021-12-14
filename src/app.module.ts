aadca5342b3cdd00df335851f14b5c6ef4b9132b
Serene <se-rene@outlook.com>
2021-12-15 04:31:31 +0800

    feat: add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { Connection } from 'typeorm';
+import { TerminusModule } from '@nestjs/terminus';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
+import { Connection } from 'typeorm';
+import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
-import { LocationsModule } from './locations/locations.module';
 import { BookingsModule } from './bookings/bookings.module';
 import commonConfig from './config/common.config';
+import { LocationsModule } from './locations/locations.module';
+import { UsersModule } from './users/users.module';
+import { HealthController } from './health/health.controller';
 
 @Module({
   imports: [
@@ -17,12 +19,13 @@ import commonConfig from './config/common.config';
       useFactory: (config: ConfigService) => config.get('database'),
       inject: [ConfigService],
     }),
+    TerminusModule,
     UsersModule,
     AuthModule,
     LocationsModule,
     BookingsModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
 })
 export class AppModule {
   constructor(private connection: Connection) {}
